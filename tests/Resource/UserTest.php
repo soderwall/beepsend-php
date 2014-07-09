ab8a8a20a627e1c50af035aba9fd646cb7b790d2
Dragan Mitic <dragan.mitic@htec.rs>
2014-07-09 11:41:15 +0200

    Created test for updating user data

--- tests/Resource/UserTest.php
+++ tests/Resource/UserTest.php
@@ -38,4 +38,36 @@ class UserTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('abc123', $user['api_token']);
     }
     
+    /**
+     * Test updating some of user data
+     */
+    public function testUpdatingData()
+    {
+        $connector = \Mockery::mock(new Curl());
+        $connector->shouldReceive('call')
+                    ->with('https://api.beepsend.com/2/users/me', 'PUT', array('name' => 'New Beepsend'))
+                    ->once()
+                    ->andReturn(array(
+                        'info' => array(
+                            'http_code' => 200,
+                            'Content-Type' => 'application/json'
+                        ),
+                        'response' => json_encode(array(
+                            'id' => 4,
+                            'name' => 'New Beepsend',
+                            'customer' => 'Beepsend AB',
+                            'api_token' => 'abc123'
+                        ))
+                    ));
+        
+        $client = new Client('abc123', $connector);
+        $user = $client->user->update(array('name' => 'New Beepsend'));
+        
+        $this->assertInternalType('array', $user);
+        $this->assertEquals(4, $user['id']);
+        $this->assertEquals('New Beepsend', $user['name']);
+        $this->assertEquals('Beepsend AB', $user['customer']);
+        $this->assertEquals('abc123', $user['api_token']);
+    }
+    
 }
