8e7008124fa84d782f7e156dde372fd5ea282bbb
Dragan Mitic <dragan.mitic@htec.rs>
2014-07-09 14:52:38 +0200

    Created test for testing getting analytics network

--- tests/Resource/AnalyticsTest.php
+++ tests/Resource/AnalyticsTest.php
@@ -34,4 +34,44 @@ class AnalyticsTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(100, $analytics['count']);
         $this->assertEquals(4.38, $analytics['price']);
     }
+    
+    /**
+     * Test getting granular delivery statistics for all messages sorted by each recipient network between two specified dates
+     */
+    public function testNetwork()
+    {
+        $connector = \Mockery::mock(new Curl());
+        $connector->shouldReceive('call')
+                    ->with('https://api.beepsend.com/2/analytics/network/me', 'GET', array())
+                    ->once()
+                    ->andReturn(array(
+                        'info' => array(
+                            'http_code' => 200,
+                            'Content-Type' => 'application/json'
+                        ),
+                        'response' => json_encode(array(
+                            'mccmnc' => array(
+                                'mcc' => 683,
+                                'mnc' => 03
+                            ),
+                            'statistics' => array(
+                                'delivered' => 100,
+                                'expired' => 12
+                            ),
+                            'total' => 142
+                        ))
+                    ));
+        
+        $client = new Client('abc123', $connector);
+        $analytics = $client->analytic->network('me');
+        
+        $this->assertInternalType('array', $analytics);
+        $this->assertInternalType('array', $analytics['mccmnc']);
+        $this->assertInternalType('array', $analytics['statistics']);
+        $this->assertEquals(683, $analytics['mccmnc']['mcc']);
+        $this->assertEquals(03, $analytics['mccmnc']['mnc']);
+        $this->assertEquals(100, $analytics['statistics']['delivered']);
+        $this->assertEquals(12, $analytics['statistics']['expired']);
+        $this->assertEquals(142, $analytics['total']);
+    }
 }
