8ba7094ebf8aa7eaaa251bf7eeea58e18c0e46ae
Dragan Mitic <dragan.mitic@htec.rs>
2014-07-10 11:33:55 +0200

    Created test for testing sending binary messages

--- tests/Resource/MessageTest.php
+++ tests/Resource/MessageTest.php
@@ -175,4 +175,42 @@ class MessageTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(null, $message[0]['errors']);
     }
     
+    /**
+     * Test sending binary messages
+     */
+    public function testBinary()
+    {
+        $connector = \Mockery::mock(new Curl());
+        $connector->shouldReceive('call')
+                    ->with(BASE_API_URL . '/' . API_VERSION . '/sms/', 'POST', array(
+                        'from' => 'Beepsend',
+                        'to' => 46736007518,
+                        'message' => 'Binary world',
+                        'receive_dlr' => 0,
+                        'message_type' => 'binary'
+                    ))
+                    ->once()
+                    ->andReturn(array(
+                        'info' => array(
+                            'http_code' => 200,
+                            'Content-Type' => 'application/json'
+                        ),
+                        'response' => json_encode(array(
+                            'id' => 07595980013893439611559146736007518,
+                            'to' => 46736007518,
+                            'from' => 'Beepsend',
+                            'error' => null
+                        ))
+                    ));
+        
+        $client = new Client('abc123', $connector);
+        $message = $client->message->binary(46736007518, 'Beepsend', 'Binary world');
+        
+        $this->assertInternalType('array', $message);
+        $this->assertEquals(07595980013893439611559146736007518, $message['id']);
+        $this->assertEquals(46736007518, $message['to']);
+        $this->assertEquals('Beepsend', $message['from']);
+        $this->assertEquals(null, $message['error']);
+    }
+    
 }
